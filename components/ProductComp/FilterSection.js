acfb8f813d06990bffed19b2928fb69d4af6cf4b
vikash0p <vikash2000158@gmail.com>
2024-02-08 11:58:52 +0530

    create a form for input text

--- components/ProductComp/FilterSection.js
+++ components/ProductComp/FilterSection.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function FilterSection() {
   return (
     <div>
-      
+      <form action="">
+        <label htmlFor="text"></label>
+        <input
+          type="text"
+          name="text"
+          id="text"
+          className='w-full h-10 px-3 bg-slate-100'
+          placeholder='SEARCH'
+        />
+      </form>
     </div>
   )
 }
